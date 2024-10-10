81885c4b805f9b74ce0fae7cc0cc32b1a27cc515
Kevin Lima <kevineziquiellima160@gmail.com>
2024-10-10 08:40:49 -0300

    Feat: Server on

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const serverPort = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+
+app.listen(serverPort, () => {
+    console.log(`🦈 Server On!:${serverPort}`);
+});
