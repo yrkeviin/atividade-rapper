e0b878fcb94ca36ecfcfb0089b4a1590edfeac48
Kevin Lima <kevineziquiellima160@gmail.com>
2024-10-10 09:13:46 -0300

    Feat: Create array descricaoFisica

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -9,18 +9,20 @@ let suspeitos = [
     nome: "Matuê",
     idade: 30,
     envolvimento: false,
+    descricaoFisica: [ "cabelo preto", "pardo", "dread" ],
 },
 {
     id: Math.floor(Math.random() * 1000000),
     nome: "Travis Scott",
     idade: 33,
     envolvimento: false,
+    descricaoFisica: [ "cabelo preto", "negro", "cabelo crespo" ],
 },
 {
     id: Math.floor(Math.random() * 1000000),
     nome: "Wiu",
     idade: 22,
     envolvimento: true,
+    descricaoFisica: [ "cabelo loiro", "branco", "dread" ],
 },
 ];
-
